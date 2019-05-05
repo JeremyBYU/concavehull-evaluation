4733c32d7560e65fa4072bb2b9e704b663529b14
Jeremy Castagno <JeremyBYU@gmail.com>
2019-05-05 15:47:52 -0400

    Adding flux script

---
+++ flux/complexity.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# The interpreter used to execute the script
+
+#“#SBATCH” directives that convey submission options:
+
+#SBATCH --job-name=polylidar+complexity
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=8000m 
+#SBATCH --time=10:00
+#SBATCH --account=jdcasta
+#SBATCH --partition=standard
+
+# The application(s) to execute along with its input arguments and options:
+
+source activate concave;
+python ./scripts/complexity_polylidar.py
---
+++ scripts/complexity_polylidar.py
@@ -0,0 +1,38 @@
+import math
+import numpy as np
+import pandas as pd
+from polylidar import extractPlanesAndPolygons, extractPolygonsAndTimings
+
+def gen_points(xmin=0, xmax=10, xstep=1, ymin=0, ymax=10, ystep=1):
+    X, Y = np.mgrid[xmin:xmax:xstep, ymin:ymax:ystep]
+    positions = np.vstack([X.ravel(), Y.ravel()])
+    positions = positions.transpose().copy()
+    return positions
+
+
+n_val_bad=[25_000, 50_000, 100_000, 200_000, 400_000, 500_000, 650_000, 800_000, 1_000_000, 1_200_000, 1_600_000, 2_000_000, 2_500_000, 3_200_000, 6_400_000]
+n_val_good=[25_000, 50_000, 100_000, 200_000, 400_000, 800_000, 1_600_000, 3_200_000, 6_400_000, 12_800_000, 25_600_000]
+n_val_test=[25_000, 50_000, 100_000]
+def polylidar_timings(reps=3, n_val=n_val_good):
+    records = []
+    polylidar_kwargs = dict(xyThresh=0.0, alpha=2.0)
+    for j in range(reps):
+        for n in n_val:
+            valmax = int(math.sqrt(n))
+            points = gen_points(xmax=valmax, ymax=valmax)
+            true_n = points.shape[0]
+            polygons, times = extractPolygonsAndTimings(points, **polylidar_kwargs)
+            records.append(dict( n=true_n, delaunay=times[0], region=times[1], polygon=times[2]))
+    return records
+
+
+def main():
+    records = polylidar_timings(reps=3, n_val=n_val_test)
+    df = pd.DataFrame.from_records(records)
+    df.to_csv("./analysis/polylidar_complexity.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
+
+# records = union_timings()
